c6d00996e06326b33e9a0a8b888ed529d951ffd0
mouadJebilou <mouadjebiloupr@gmail.com>
2023-03-27 03:46:12 -0700

    TASK 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,12 @@
-void reset_to_98(int *n) {
-    *n = 98;
+/**
+ * reset_to_98 - Met à jour la valeur d'un entier pointé vers 98.
+ * @n: Pointeur vers l'entier dont la valeur doit être mise à jour.
+ *
+ * Description: Cette fonction prend un pointeur vers un entier en paramètre
+ * et met à jour la valeur de cet entier à 98.
+ * Return: Aucun.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
