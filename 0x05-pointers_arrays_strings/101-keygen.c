4ea1013343b22e05e2e8af05b908731e669e63c2
mouadJebilou <mouadjebiloupr@gmail.com>
2023-03-27 15:38:48 -0700

    TASK 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,25 +3,27 @@
 #include <time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Entry point.
+ * Description: Generates random valid
+ * passwords for the 101-crackme program.
+ * Return: 0 (success)
  */
 int main(void)
 {
-    int i, len = 8;
-    char password[9];
+    int i;
+    int random_value;
+    int password_length = 10; // You can change the password length as needed.
+    unsigned int seed = time(NULL);
 
-    srand(time(NULL));
+    srand(seed);
 
-    for (i = 0; i < len; i++)
+    for (i = 0; i < password_length; i++)
     {
-        password[i] = rand() % 94 + 32;
+        random_value = (rand() % 94) + 33; // Generate a random value between 33 and 126 (inclusive).
+        printf("%c", random_value);
     }
 
-    password[i] = '\0';
-
-    printf("%s\n", password);
+    printf("\n");
 
     return (0);
 }
