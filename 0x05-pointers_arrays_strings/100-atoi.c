fd998af2ed68444c69ac5deb0f52d6a1ca29901c
mouadJebilou <mouadjebiloupr@gmail.com>
2023-03-27 16:28:29 -0700

    TASK 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,48 +1,32 @@
+#include "main.h"
+
 /**
- * _atoi - Convertit une chaîne de caractères en un entier.
- * @s: Pointeur vers la chaîne de caractères à convertir.
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
  *
- * Description: Cette fonction prend en paramètre un
- * pointeur vers une chaîne de caractères et convertit
- * la première séquence de chiffres trouvée en un
- * entier. Elle prend également en compte les signes
- * + et -, ainsi que les caractères qui précèdent
- * la séquence de chiffres. La fonction renvoie l'entier
- * converti. Si aucun chiffre n'est trouvé dans
- * la chaîne, la fonction renvoie 0.
- *
- * Return: L'entier converti.
+ * Return: The integer value of the string,
+ * or 0 if no number is found
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+    int result = 0;
+    int sign = 1;
+    int started_parsing = 0;
 
-	/* Vérifie le signe */
-	if (s[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[0] == '+')
-	{
-		i++;
-	}
+    while (*s)
+    {
+        if (*s == '-')
+            sign *= -1;
+        else if (*s >= '0' && *s <= '9')
+        {
+            result = result * 10 + (*s - '0');
+            started_parsing = 1;
+        }
+        else if (started_parsing)
+            break;
+        
+        s++;
+    }
 
-	/* Convertit chaque chiffre */
-	while (s[i] != '\0')
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-		}
-		else
-		{
-			break;
-		}
-		i++;
-	}
-
-	return (result * sign);
+    return result * sign;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 
 int _putchar(char c);
 void _puts(char *str);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
