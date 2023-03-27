8db435649fa143d3833f19e888f7aa90300214be
mouadJebilou <mouadjebiloupr@gmail.com>
2023-03-27 04:18:13 -0700

    TASK 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,9 @@
  * _strlen - Calcule la longueur d'une chaîne de caractères.
  * @s: Pointeur vers la chaîne de caractères.
  *
- * Description: Cette fonction prend un pointeur vers une chaîne de caractères
- * et calcule la longueur de cette chaîne en comptant le nombre de caractères
- * avant le caractère nul de fin de chaîne.
+ * Description: Cette fonction prend un pointeur vers une chaîne de
+ * caractères  et calcule la longueur de cette chaîne en comptant le
+ * nombre de caractères avant le caractère nul de fin de chaîne.
  *
  * Return: La longueur de la chaîne de caractères.
  */
