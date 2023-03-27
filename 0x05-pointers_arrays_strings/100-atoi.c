d3c0eb4b93ae907c12e0da1de4f629efdc4346f4
mouadJebilou <mouadjebiloupr@gmail.com>
2023-03-27 16:42:51 -0700

    TASK 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,37 +5,36 @@
  * _atoi - Converts a string to an integer
  * @s: The string to convert
  *
- * Return: The integer value of the string,
- * or 0 if no number is found
+ * Return: The integer value of the string, or 0 if no number is found
  */
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int started_parsing = 0;
+	int result = 0;
+	int sign = 1;
+	int started_parsing = 0;
 
-    while (*s)
-    {
-        if (*s == '-')
-            sign *= -1;
-        else if (*s >= '0' && *s <= '9')
-        {
-            if (result > (INT_MAX - (*s - '0')) / 10)
-            {
-                if (sign > 0)
-                    return INT_MAX;
-                else
-                    return INT_MIN;
-            }
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+		{
+			if (result > (INT_MAX - (*s - '0')) / 10)
+			{
+				if (sign > 0)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
 
-            result = result * 10 + (*s - '0');
-            started_parsing = 1;
-        }
-        else if (started_parsing)
-            break;
-        
-        s++;
-    }
+			result = result * 10 + (*s - '0');
+			started_parsing = 1;
+		}
+		else if (started_parsing)
+			break;
 
-    return result * sign;
+		s++;
+	}
+
+	return (result * sign);
 }
