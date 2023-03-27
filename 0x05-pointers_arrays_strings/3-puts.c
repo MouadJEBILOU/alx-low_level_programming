b5cc71eb57ef88cac2ae4363e45056c54751e499
mouadJebilou <mouadjebiloupr@gmail.com>
2023-03-27 04:36:53 -0700

    TASK 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+/**
+ * _puts - Imprime une chaîne de caractères suivie d'un saut de ligne.
+ * @str: Pointeur vers la chaîne de caractères à imprimer.
+ *
+ * Description: Cette fonction prend en paramètre un pointeur vers une chaîne
+ * de caractères et l'imprime sur la sortie standard, suivi d'un saut de ligne.
+ *
+ * Return: Aucun.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
