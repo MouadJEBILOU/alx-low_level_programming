08d596aeddbd617991cd083adf226f78e73898d6
mouadJebilou <mouadjebiloupr@gmail.com>
2023-04-02 13:33:24 -0700

    TASK 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,22 +7,22 @@
  */
 int main(void)
 {
-    int num1 = 1, num2 = 2, next = 0;
-    int sum = 2;
+	int num1 = 1, num2 = 2, next = 0;
+	int sum = 2;
 
-    while (next < 4000000)
-    {
-        next = num1 + num2;
-        num1 = num2;
-        num2 = next;
+	while (next < 4000000)
+	{
+		next = num1 + num2;
+		num1 = num2;
+		num2 = next;
 
-        if (next % 2 == 0)
-        {
-            sum += next;
-        }
-    }
+		if (next % 2 == 0)
+		{
+			sum += next;
+		}
+	}
 
-    printf("%d\n", sum);
+	printf("%d\n", sum);
 
-    return (0);
+	return 0;
 }
