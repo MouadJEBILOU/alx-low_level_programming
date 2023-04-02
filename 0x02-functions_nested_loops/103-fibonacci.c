bc33b2a63a2a00464a0b9a917556747604de87e6
mouadJebilou <mouadjebiloupr@gmail.com>
2023-04-02 13:25:54 -0700

    TASK 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int num1 = 1, num2 = 2, next = 0;
+    int sum = 2;
+
+    while (next < 4000000)
+    {
+        next = num1 + num2;
+        num1 = num2;
+        num2 = next;
+
+        if (next % 2 == 0)
+        {
+            sum += next;
+        }
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
