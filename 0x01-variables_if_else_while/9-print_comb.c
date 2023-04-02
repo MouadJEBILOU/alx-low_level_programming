e893925388091443e3f19a9bb57d7828f0a015ec
mouadJebilou <mouadjebiloupr@gmail.com>
2023-04-02 08:55:47 -0700

    TASK 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,22 +7,19 @@
  */
 int main(void)
 {
-    int i, j;
+	int num;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = i + 1; j < 10; j++)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
 
-            if (i != 8 || j != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
