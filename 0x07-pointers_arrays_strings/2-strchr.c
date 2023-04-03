3be79cfc7cfcd9df0d16821b8d4019713fb98acd
mouadJebilou <mouadjebiloupr@gmail.com>
2023-04-03 13:55:55 -0700

    TASK 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,19 +11,19 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s)
-    {
-        if (*s == c)
-        {
-            return (s);
-        }
-        s++;
-    }
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
 
-    if (c == '\0')
-    {
-        return (s);
-    }
+	if (c == '\0')
+	{
+		return (s);
+	}
 
-    return (NULL);
+	return (NULL);
 }
