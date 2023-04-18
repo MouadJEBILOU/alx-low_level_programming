fa221f24962ee9e506d8d2edb2c9243c6c48d5c8
MouadJEBILOU <mouad.jebilou1@gmail.com>
2023-04-18 22:48:35 +0000

    FIRST TASK

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct containing dog info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: struct containing information about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
