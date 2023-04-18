27496804526f1a5154078bf17f1429173214a52f
MouadJEBILOU <mouad.jebilou1@gmail.com>
2023-04-18 23:37:42 +0000

    TASK 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+/**
+ * free_dog - Frees a dog structure
+ * @d: Pointer to dog structure to free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+    if (d != NULL)
+    {
+        free(d->name);
+        free(d->owner);
+        free(d);
+    }
+}
