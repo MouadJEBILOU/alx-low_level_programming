b561fa9fb074090a53478a1d33936cb12f003f8d
MouadJEBILOU <mouad.jebilou1@gmail.com>
2023-04-19 00:36:35 +0000

    TASK 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,7 @@
+/* File: 0-main.c */
+
 #include <stdio.h>
 
-/**
- * main - entry point of the program
- *
- * Description: This function prints
- * the name of the file it was compiled from
- * Return: 0, always successfull
- */
 int main(void)
 {
     printf("%s\n", __FILE__);
