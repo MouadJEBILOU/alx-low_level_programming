ba6765134b3b85fd84cdd963e5f5a7fa90ae94e8
MouadJEBILOU <mouad.jebilou1@gmail.com>
2023-04-21 01:54:35 +0000

    TASK 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,82 +6,93 @@
 /**
  * print_c - prints a character
  * @args: va_list containing the character to print
+ *
+ * Return: void
  */
 void print_c(va_list args)
 {
-    printf("%c", va_arg(args, int));
+	printf("%c", va_arg(args, int));
 }
 
 /**
  * print_i - prints an integer
  * @args: va_list containing the integer to print
+ *
+ * Return: void
  */
 void print_i(va_list args)
 {
-    printf("%d", va_arg(args, int));
+	printf("%d", va_arg(args, int));
 }
 
 /**
  * print_f - prints a float
  * @args: va_list containing the float to print
+ *
+ * Return: void
  */
 void print_f(va_list args)
 {
-    printf("%f", va_arg(args, double));
+	printf("%f", va_arg(args, double));
 }
 
 /**
  * print_s - prints a string
  * @args: va_list containing the string to print
+ *
+ * Return: void
  */
 void print_s(va_list args)
 {
-    char *str = va_arg(args, char *);
-    if (str == NULL)
-    {
-        printf("(nil)");
-        return;
-    }
-    printf("%s", str);
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
 }
 
 /**
  * print_all - prints anything
  * @format: a list of types of arguments passed to the function
+ *
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int i = 0, j = 0;
-    char *sep = "";
-    print_fn_t funcs[] = {
-        {'c', print_c},
-        {'i', print_i},
-        {'f', print_f},
-        {'s', print_s},
-        {'\0', NULL}
-    };
+	va_list args;
+	int i = 0, j = 0;
+	char *sep = "";
+	print_fn_t funcs[] = {
+		{'c', print_c},
+		{'i', print_i},
+		{'f', print_f},
+		{'s', print_s},
+		{'\0', NULL}
+	};
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[i])
-    {
-        j = 0;
-        while (funcs[j].type)
-        {
-            if (funcs[j].type == format[i])
-            {
-                printf("%s", sep);
-                funcs[j].fn(args);
-                sep = ", ";
-                break;
-            }
-            j++;
-        }
-        i++;
-    }
+	while (format && format[i])
+	{
+		j = 0;
+		while (funcs[j].type)
+		{
+			if (funcs[j].type == format[i])
+			{
+				printf("%s", sep);
+				funcs[j].fn(args);
+				sep = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
 
-    printf("\n");
+	printf("\n");
 
-    va_end(args);
+	va_end(args);
 }
