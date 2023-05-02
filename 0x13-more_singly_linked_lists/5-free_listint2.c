35c8e2c436b78af96f1b23516bd23b49f0a7fa5d
MouadJEBILOU <mouad.jebilou1@gmail.com>
2023-05-03 00:29:27 +0100

    TASK 0 - 1 - 2 - 3 - 4 - 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
