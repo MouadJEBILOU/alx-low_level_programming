a6e50abc5bbdce5937f02f28d39b8ab8295fd48f
MouadJEBILOU <mouad.jebilou1@gmail.com>
2023-05-03 01:10:51 +0100

    TASK 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,20 @@
 #include "lists.h"
 
 /**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: head node of the linked list
  *
- * Return: the number of elements
- *
+ * Return: number of nodes in the list
  */
-
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-	h = h->next;
 		count++;
+		h = h->next;
 	}
+
 	return (count);
 }
